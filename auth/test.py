af84b75458809c820f7d6b035c7ce6f49d5a56d1
NethDR <mihai.udubasa@yahoo.com>
2023-05-20 12:26:03 +0300

    fix routes on test.py

--- auth/test.py
+++ auth/test.py
@@ -1,11 +1,12 @@
 import requests, hashlib
 
 # url = 'http://127.0.0.1:5000/login'
-url = 'http://127.0.0.1:5000/register'
+# url = 'http://127.0.0.1:8000/auth/register'
+url = 'http://127.0.0.1:8000/auth/login'
 
 print(
     requests.post(url, json={
-        'username': 'barbie', 
+        'username': 'barbie',
         'password': hashlib.sha256('barbie'.encode('utf-8')).hexdigest()
     }).status_code
 )
